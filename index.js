527e22428a132b6b496854c9efa7217b8b66ed63
Orin Serimon <orin220444@protonmail.com>
2020-03-29 20:43:25 +0700

    start working

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const Telegraf = require('telegraf')
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.on('message')
+const Telegraf = require('telegraf');
+
+require('dotenv').config({path: './.env'});
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const {
+  handleStart,
+  handleAdd,
+  handleAuth
+} = require('./handlers');
+bot.start(handleStart);
+// bot.on('message', handleAdd);
+bot.command('auth', handleAuth);
+bot.launch();
